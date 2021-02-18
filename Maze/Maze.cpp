52137f76c8072e351fa55baf1f00c62702ddd675
Andrew Kim <66102642+ahkim3@users.noreply.github.com>
2021-02-18 06:07:15 -0600

    Create skeleton of program

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -16,6 +16,10 @@ using namespace std;
 
 const enum ENTRANCE { X = 0, Y = 2 };
 
+void mazeTraverse(char[12][12], int, int, int, int);
+bool validMove(char[12][12], int, int);
+bool isSolved(int, int, int);
+
 
 int main()
 {
@@ -52,4 +56,25 @@ int main()
             cout << maze[i][j] << ' ';
         cout << endl;
     }
+}
+
+
+// Executes tasks for each step in maze, marking an X at current position
+void mazeTraverse(char maze[12][12], int size, int xCurrent, int yCurrent, 
+    int nextDirection)
+{
+}
+
+
+// Determine if moving to a specified position in a maze is allowed
+bool validMove(char maze[12][12], int xCoord, int yCoord)
+{
+    return false;
+}
+
+
+// Determines if exit of maze has been found
+bool isSolved(int size, int xCoord, int yCoord)
+{
+    return false;
 }
